2205c06af03a8ba57b2ed303500ec249ac773c9e
wildan <muhammadwildaniskandar@gmail.com>
2017-03-17 21:16:56 +0700

    Update UI Tambah Aspirasi

--- app/src/main/java/com/example/pattimura/sundawenang/Fragment/Aspirasi.java
+++ app/src/main/java/com/example/pattimura/sundawenang/Fragment/Aspirasi.java
@@ -2,7 +2,9 @@ package com.example.pattimura.sundawenang.Fragment;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +15,7 @@ import com.example.pattimura.sundawenang.R;
  * A simple {@link Fragment} subclass.
  */
 public class Aspirasi extends Fragment {
-
+    Fragment fragment;
 
     public Aspirasi() {
         // Required empty public constructor
@@ -24,7 +26,20 @@ public class Aspirasi extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_aspirasi, container, false);
+        View v =  inflater.inflate(R.layout.fragment_aspirasi, container, false);
+        FloatingActionButton fab = (FloatingActionButton) v.findViewById(R.id.floatingActionButton);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragment = new TambahAspirasi();
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.mainframe, fragment);
+                ft.commit();
+            }
+        });
+
+        return v;
     }
 
 }
